327043cae940531156cf50c5d40ca2f42afb2c02
hellisoued <30546516+hellisoued@users.noreply.github.com>
2022-03-31 04:09:02 +0900

    프로젝트 구조 개선 - 1차
    주요 수정사항
    NEIS OpenAPI 요청부 scripts\lunch_api.py로 분리
    
    부수적 수정사항
    .gitignore 수정
    api 관련 디버그 및 관리 편의성 개선

--- main.py
+++ main.py
@@ -1,66 +1,39 @@
-import re
 from tkinter import *
 from tkinter import ttk
-import requests
-
 from datetime import datetime, timedelta
 import schedule as schedule
 import time
+from scripts.lunch_api import lunch_api
+
 root = Tk()
 root.title('Diet')
 main = ('Arial', 70)
 small = ('Arial', 45)
+
+lunch = lunch_api()
 # import sys
 # from PyQt5.QtWidgets import QAplication, QWidget
 
-def update():
-    global date, req
-    date = datetime.today()
-    serviceKey = '128332e82a2f42bcbc58d826a24084ce'
-    url = 'https://open.neis.go.kr/hub/mealServiceDietInfo'
-    params = {'Key': serviceKey,
-              'Type': 'json',
-              'pIndex': 1,
-              'pSize': 5,
-              'ATPT_OFCDC_SC_CODE': 'J10',
-              'SD_SCHUL_CODE': 7530544,
-              'MLSV_FROM_YMD': date.strftime('%Y%m%d'),
-              'MLSV_TO_YMD': (date + timedelta(days=2)).strftime('%Y%m%d')
-              }
-    print(params)
-    req = requests.get(url, params=params).json()['mealServiceDietInfo']
-
-def dish(i):
-    tmp = req[1]['row'][i]['DDISH_NM']
-    tmp = re.sub(r'[0-9*.<br>]+', '', tmp)
-    tmp = re.sub(r'[/]+', '\n', tmp)
-    cal = req[1]['row'][i]['CAL_INFO']
-    return {'diet': tmp, 'cal': cal}
 def display():
     frm = ttk.Frame(root, padding=20)
     frm.pack(expand=YES, fill=BOTH)
     frm.grid(column=0, row=0)
-    ttk.Label(frm, text=date.strftime('%m.%d'), font=small).grid(column=0, row=0)
-    ttk.Label(frm, text=(date + timedelta(days=1)).strftime('%m.%d'), font=small).grid(column=2, row=0)
-    ttk.Label(frm, text=dish(0)['diet'], padding=10, font=main, anchor=N).grid(column=0, row=1)
+    ttk.Label(frm, text=lunch.date.strftime('%m.%d'), font=small).grid(column=0, row=0)
+    ttk.Label(frm, text=(lunch.date + timedelta(days=1)).strftime('%m.%d'), font=small).grid(column=2, row=0)
+    ttk.Label(frm, text=lunch.dish[0], padding=10, font=main, anchor=N).grid(column=0, row=1)
     ttk.Label(frm, text='>', padding=10, font=main, anchor=N).grid(column=1, row=1)
-    ttk.Label(frm, text=dish(1)['diet'], padding=10, font=main, anchor=N).grid(column=2, row=1)
+    ttk.Label(frm, text=lunch.dish[1], padding=10, font=main, anchor=N).grid(column=2, row=1)
     root.mainloop()
 
-schedule.every(10).seconds.do(update)
+display()
+
+'''
+schedule.every(10).seconds.do(lunch.api_call())
 schedule.every(11).seconds.do(dish, 1)
 schedule.every(12).seconds.do(dish, 2)
 schedule.every(13).seconds.do(display)
 while True:
     schedule.run_pending()
     time.sleep(1)
-
-
+'''
 # schedule.every().day.at("7:00").do(update)
-
-
-
-
-
-
-
---
+++ scripts/lunch_api.py
@@ -0,0 +1,53 @@
+from datetime import datetime, timedelta
+import copy
+import requests
+import re
+
+url = 'https://open.neis.go.kr/hub/mealServiceDietInfo'
+serviceKey = '128332e82a2f42bcbc58d826a24084ce'
+ATPT_OFCDC_SC_CODE = 'J10'
+SD_SCHUL_CODE = 7530544
+
+query_template = {
+        'Key': serviceKey,
+        'Type': 'json',
+        'pIndex': 1,
+        'pSize': 2,
+        'ATPT_OFCDC_SC_CODE': ATPT_OFCDC_SC_CODE,
+        'SD_SCHUL_CODE': SD_SCHUL_CODE,
+        'MMEAL_SC_CODE': '',
+        'MLSV_FROM_YMD': '',
+        'MLSV_TO_YMD': '',
+    }
+
+class lunch_api():
+    def __init__(self, data=None, date=None, dish=None, cal=None):
+        self.api_call()
+
+    def api_call(self):
+        self.date = datetime.today()
+        query = copy.deepcopy(query_template)
+        query['MLSV_FROM_YMD'] = self.date.strftime('%Y%m%d')
+        query['MLSV_TO_YMD'] = (self.date + timedelta(days=1)).strftime('%Y%m%d')
+        res = requests.get(url, params=query).json()
+
+        try:
+            self.data = res['mealServiceDietInfo']
+
+        except KeyError:
+            CODE = res['RESULT']['CODE']
+            MESSAGE = res['RESULT']['MESSAGE']
+            print('<API CALL FAILURE> {}: {}'.format(CODE, MESSAGE))
+            return 1
+
+        else:
+            CODE = self.data[0]['head'][1]['RESULT']['CODE']
+            MESSAGE = self.data[0]['head'][1]['RESULT']['MESSAGE']
+            print('<API CALL SUCCESS> {}: {}'.format(CODE, MESSAGE))
+            self.dish = ['']*query['pSize']
+            self.cal = ['']*query['pSize']
+            for i in range(query['pSize']):
+                self.dish[i] = self.data[1]['row'][i]['DDISH_NM']
+                self.dish[i] = re.sub(r'[0-9*.<br>]+', '', self.dish[i])
+                self.dish[i] = re.sub(r'[/]+', '\n', self.dish[i])
+                self.cal[i] = self.data[1]['row'][i]['CAL_INFO']
