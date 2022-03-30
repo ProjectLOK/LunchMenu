fcc3c33b2b02cfb2dae117c30bbb5001e126ee4e
DROP-DATABASE-M <sh11ve@naver.com>
2022-03-30 23:50:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import re
 from tkinter import *
 from tkinter import ttk
 import requests
-# import sys
-# from PyQt5.QtWidgets import QAplication, QWidget
+
 from datetime import datetime, timedelta
 import schedule as schedule
 import time
-
+root = Tk()
+root.title('Diet')
+main = ('Arial', 70)
+small = ('Arial', 45)
+# import sys
+# from PyQt5.QtWidgets import QAplication, QWidget
 
 def update():
     global date, req
@@ -26,36 +30,37 @@ def update():
     print(params)
     req = requests.get(url, params=params).json()['mealServiceDietInfo']
 
-
-schedule.every(24).hours.do(update())
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
-
-
-# schedule.every().day.at("7:00").do(update())
-
-
 def dish(i):
     tmp = req[1]['row'][i]['DDISH_NM']
     tmp = re.sub(r'[0-9*.<br>]+', '', tmp)
     tmp = re.sub(r'[/]+', '\n', tmp)
     cal = req[1]['row'][i]['CAL_INFO']
     return {'diet': tmp, 'cal': cal}
+def display():
+    frm = ttk.Frame(root, padding=20)
+    frm.pack(expand=YES, fill=BOTH)
+    frm.grid(column=0, row=0)
+    ttk.Label(frm, text=date.strftime('%m.%d'), font=small).grid(column=0, row=0)
+    ttk.Label(frm, text=(date + timedelta(days=1)).strftime('%m.%d'), font=small).grid(column=2, row=0)
+    ttk.Label(frm, text=dish(0)['diet'], padding=10, font=main, anchor=N).grid(column=0, row=1)
+    ttk.Label(frm, text='>', padding=10, font=main, anchor=N).grid(column=1, row=1)
+    ttk.Label(frm, text=dish(1)['diet'], padding=10, font=main, anchor=N).grid(column=2, row=1)
+    root.mainloop()
+
+schedule.every(10).seconds.do(update)
+schedule.every(11).seconds.do(dish, 1)
+schedule.every(12).seconds.do(dish, 2)
+schedule.every(13).seconds.do(display)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
 
-root = Tk()
-root.title('Diet')
-main = ('Arial', 70)
-small = ('Arial', 45)
+# schedule.every().day.at("7:00").do(update)
 
-frm = ttk.Frame(root, padding=20)
-frm.pack(expand=YES, fill=BOTH)
-frm.grid(column=0, row=0)
-ttk.Label(frm, text=date.strftime('%m.%d'), font=small).grid(column=0, row=0)
-ttk.Label(frm, text=(date + timedelta(days=1)).strftime('%m.%d'), font=small).grid(column=2, row=0)
-ttk.Label(frm, text=dish(0)['diet'], padding=10, font=main, anchor=N).grid(column=0, row=1)
-ttk.Label(frm, text='>', padding=10, font=main, anchor=N).grid(column=1, row=1)
-ttk.Label(frm, text=dish(1)['diet'], padding=10, font=main, anchor=N).grid(column=2, row=1)
-root.mainloop()
+
+
+
+
+
+
