b3c9404fce314f4752a0845293a64f62d4226f85
DROP-DATABASE-M <sh11ve@naver.com>
2022-04-05 22:42:19 +0900

    Revert "async complete"
    This reverts commit e268db49e96d316f4254e34e6d62b96dd56235ca.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import time
 from tkinter import *
 from tkinter import ttk
 from scripts.lunch_api import lunch_api
@@ -9,40 +8,34 @@ root.title('Diet')
 main = ('Arial', 70)
 small = ('Arial', 45)
 lunch = lunch_api()
+
 frm = ttk.Frame(root, padding=20)
 date_today = ttk.Label(frm, text=lunch.date[0], font=small)
 date_next = ttk.Label(frm, text=lunch.date[1], font=small)
 dish_today = ttk.Label(frm, text=lunch.dish[0], padding=10, font=main, anchor=N)
 dish_next = ttk.Label(frm, text=lunch.dish[1], padding=10, font=main, anchor=N)
-date_today.grid(column=0, row=0)
-date_next.grid(column=2, row=0)
-dish_today.grid(column=0, row=1)
-dish_next.grid(column=2, row=1)
-ttk.Label(frm, text='>', padding=10, font=main, anchor=N).grid(column=1, row=1)
-frm.pack(expand=YES, fill=BOTH)
 
 
-async def main():
-    gui_task = asyncio.create_task(GUI())
-    while True:
-        await update()
-
-async def GUI():
-    while True:
-        root.update()
-        await asyncio.sleep(0.001)
+async def display():
+    date_today.grid(column=0, row=0)
+    date_next.grid(column=2, row=0)
+    dish_today.grid(column=0, row=1)
+    dish_next.grid(column=2, row=1)
+    ttk.Label(frm, text='>', padding=10, font=main, anchor=N).grid(column=1, row=1)
+    frm.pack(expand=YES, fill=BOTH)
+    root.mainloop()
 
 
 async def update():
-    await asyncio.sleep(15)
     lunch.api_call()
-    dish_today.configure(text=lunch.dish[0])
-    dish_next.configure(text=lunch.dish[1])
-    root.update()
-    print("update success")
+    date_today.text = 'asdfas'
+    await asyncio.sleep(1000)
 
-if __name__ == '__main__':
-    asyncio.run(main())
+asyncio.run(display())
+print(1)
+while True:
+    update()
+
 
 '''
 # schedule.every().day.at("7:00").do(update)
