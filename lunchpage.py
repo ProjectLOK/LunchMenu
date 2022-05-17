be34fb3201ee3b72c74509b3abb3fab8885ec6d2
MangoDB <44537939+DROP-DATABASE-M@users.noreply.github.com>
2022-05-17 10:58:51 +0900

    Create weather page

--- lunchpage.py
+++ lunchpage.py
@@ -1,10 +1,14 @@
+import asyncio
+from tkinter import *
+from tkinter import ttk
+from scripts.lunch_api import lunch_api
+
 root = Tk()
 root.title('Diet')
 root.geometry('1872x1404')
 main = ('Arial', 70)
 small = ('Arial', 45)
 lunch = lunch_api()
-weather = weather_api()
 frm = ttk.Frame(root, padding=20)
 date_today = ttk.Label(frm, text=lunch.date[0], font=small)
 date_next = ttk.Label(frm, text=lunch.date[1], font=small)
@@ -16,3 +20,12 @@ dish_today.grid(column=0, row=1)
 dish_next.grid(column=2, row=1)
 ttk.Label(frm, text='>', padding=10, font=main, anchor=N).grid(column=1, row=1)
 frm.pack(expand=YES, fill=BOTH)
+
+
+async def update():
+    lunch.api_call()
+    dish_today.configure(text=lunch.dish[0])
+    dish_next.configure(text=lunch.dish[1])
+    date_today.configure(text=lunch.date[0])
+    date_next.configure(text=lunch.date[1])
+    print("update success")
--- main.py
+++ main.py
@@ -1,50 +1,19 @@
 import time
-from tkinter import *
-from tkinter import ttk
-from scripts.lunch_api import lunch_api
 import asyncio
 from scripts.nWeather_api import weather_api
 import aioschedule as sch
-
-root = Tk()
-root.title('Diet')
-root.geometry('1872x1404')
-main = ('Arial', 70)
-small = ('Arial', 45)
-lunch = lunch_api()
-weather = weather_api()
-frm = ttk.Frame(root, padding=20)
-date_today = ttk.Label(frm, text=lunch.date[0], font=small)
-date_next = ttk.Label(frm, text=lunch.date[1], font=small)
-dish_today = ttk.Label(frm, text=lunch.dish[0], padding=10, font=main, anchor=N)
-dish_next = ttk.Label(frm, text=lunch.dish[1], padding=10, font=main, anchor=N)
-date_today.grid(column=0, row=0)
-date_next.grid(column=2, row=0)
-dish_today.grid(column=0, row=1)
-dish_next.grid(column=2, row=1)
-ttk.Label(frm, text='>', padding=10, font=main, anchor=N).grid(column=1, row=1)
-frm.pack(expand=YES, fill=BOTH)
+import lunchpage
 
 
 async def main():
-    sch.every().day.at("03:00").do(update)
+    sch.every().day.at("03:00").do(lunchpage.update)
     await GUI()
 
 
 async def GUI():
     while True:
         await sch.run_pending()
-        root.update()
-
-
-async def update():
-    lunch.api_call()
-    dish_today.configure(text=lunch.dish[0])
-    dish_next.configure(text=lunch.dish[1])
-    date_today.configure(text=lunch.date[0])
-    date_next.configure(text=lunch.date[1])
-    print("update success")
-
+        lunchpage.root.update()
 
 if __name__ == '__main__':
     asyncio.run(main())
