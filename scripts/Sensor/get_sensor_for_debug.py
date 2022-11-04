110088bd2cc1d29dc81a0f3fb37b3c73b5648660
MangoDB <44537939+mang0db@users.noreply.github.com>
2022-11-04 16:28:44 +0900

    ..

---
+++ scripts/Sensor/get_sensor_for_debug.py
@@ -0,0 +1,33 @@
+import serial
+import time
+import json
+
+with open('config.json', 'r') as data:
+    config = data.read()
+    data.close()
+    config = json.loads(config)
+    rtSensor = config['sensor']
+if rtSensor:
+    ardu = serial.Serial("/dev/ttyUSB0", 115200)
+    time.sleep(5)
+
+    category = ['temp', 'humi', 'pm10', 'pm2.5', 'co2', 'form']
+
+
+    def getData():
+        dataset = [''] * 6
+        ardu.write(str.encode('update'))
+        # time.sleep(0.1)
+        for i in range(6):
+            #print(i)
+            data = ardu.readline()  # read until ('\n')
+            dataset[i] = data.decode('utf-8').rstrip()
+        sensor = dict(zip(category, dataset))
+        #print(sensor)
+        #print(dataset)
+        return sensor
+
+
+    getData()
+    time.sleep(0.5)
+    getData()
